cfcefd6dd554cb39ee295cae281aae11f3517fb0
Matheus Cardoso <matheuscas@Matheuss-MacBook-Pro.local>
2018-08-30 11:00:48 -0300

    rome basis routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { RiskTypeDetailsComponent } from './risktype/risk-type-details/risk-type-details.component';
+
+const appRoutes: Routes = [
+  {path: 'risktypes/:id', loadChildren: './risktype/risktype.module#RisktypeModule'}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
