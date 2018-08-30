45757a500f524c378a7a73c3000a206fb2637f38
Matheus Cardoso <matheuscas@Matheuss-MacBook-Pro.local>
2018-08-30 20:57:27 -0300

    Code to get risktype by id

--- src/app/risktype/risk-type-details/risk-type-details.component.ts
+++ src/app/risktype/risk-type-details/risk-type-details.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { RisktypesService } from '../../risktypes.service';
 
 @Component({
   selector: 'app-risk-type-details',
   templateUrl: './risk-type-details.component.html',
   styleUrls: ['./risk-type-details.component.css']
 })
-export class RiskTypeDetailsComponent implements OnInit {
+export class RiskTypeDetailsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  routeSub$: Subscription;
+  risk$: any;
+
+  constructor(private route: ActivatedRoute, public risktypesService: RisktypesService) { }
 
   ngOnInit() {
+    this.routeSub$ = this.route.params.subscribe(params => {
+      this.risk$ = this.risktypesService.get(+params['id']);
+    });
+  }
+
+  ngOnDestroy() {
+    this.routeSub$.unsubscribe();
   }
 
 }
